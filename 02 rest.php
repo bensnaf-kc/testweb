b9aae78d68ee57faaa0319ecb7181e6854d05d0a
bensnaf-kc <80310420+bensnaf-kc@users.noreply.github.com>
2021-03-19 16:08:05 +0700

    Update 02 rest.php

--- 02 rest.php
+++ 02 rest.php
@@ -1,18 +1,35 @@
-<?php
-  include_once "01 dbcontrol.php";
-  include_once "util.php";
-  $debug_mode = false;
-  if($_SERVER['REQUEST_METHOD']=='GET'){
-    get_data($debug_mode);
-  }else if($_SERVER['REQUEST_METHOD']=='POST'){
-    text_debug("POST may be support next time<br>", $debug_mode);
-  }else{
-    http_response_code(405);//Error unsuport by current version
-  }
+<?php 
+    include_once "01 dbcontrol.php";
+    include_once "util.php";
+    $debug = false;
+    if(isset($_POST["row"])){
+        
+    }
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+        echo json_encode(get_data($debug));
+    } else if($_SERVER['REQUEST_METHOD']=='POST'){
+        echo "POST may be next time<br>", $debug;
+        //echo json_encode("{'Message':"+print_r($_POST)+"}");
+        if(isset($_POST["new_id"]) && isset($_POST["new_name"])){
+            $new_id = $_POST["new_id"];
+            $new_name = $_POST["new_name"];
+            insert_newdata($new_id,$new_name,$debug);
+            echo json_encode(get_data($debug));
+        }
+    } else {
+        http_response_code(405); // ERROR unsupport by current version
+    }
+    function get_data($debug){
+        $my_db = new db("root","","book",$debug);
+        $data = $my_db->sel_query("SELECT * FROM user");
+        $my_db->close();
+        return $data;
+    }
 
-  function get_data($debug_mode){
-    $my_db = new db("root","","book",$debug_mode);
-    $data = $my_db->query("select * from user");
-    echo json_encode($data);
-  }
+    function insert_newdata($new_id,$new_name,$debug){
+        $my_db = new db("root","","book",$debug);
+        $sql = "INSERT INTO user (id,name) VALUES ({$new_id}, {$new_name})";
+        $data = $my_db->query_only($sql);
+        $my_db->close();
+    }
 ?>
